622121738bc084cce2a342f640bceeeff977e625
Qcode <87869284+qfundimity@users.noreply.github.com>
2022-06-27 16:59:25 -0400

    Update py_td_to_json.py

--- py_td_to_json.py
+++ py_td_to_json.py
@@ -9,15 +9,17 @@ Original file is located at
 
 import teradatasql
 import pandas as pd
+import json
+
 query = 'select * from ndw_ebi_dmr_shared_views.address_dim sample 50'
+
 with teradatasql.connect(host='172.28.130.20', user='kmania001c', password='EastBrunswick@2022', logmech = "LDAP", encryptdata='true') as connect:  
   data1 = pd.read_sql(query, connect)
-  
-print(data1.head())
+  print(data1.head())
 
 #write data to json file
 #Day1 - load first 10 records
-import json
+
 d3 = data2.head(10).to_dict()
 j1 = json.dumps(d3,indent=2, sort_keys= True)
 print(j1)
